8f2be610487626025fdb9e2abf30548b31651d7e
Mengni Wang <mengni.wang@intel.com>
2024-06-04 06:50:58 +0000

    fix awq
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- onnx_neural_compressor/algorithms/weight_only/awq.py
+++ onnx_neural_compressor/algorithms/weight_only/awq.py
@@ -110,7 +110,7 @@ def _apply_awq_scale(model, weight_config, absorb_pairs, output_dicts, num_bits,
                 else:
                     q_weight = quant_utils.qdq_tensor(weight, num_bits, group_size, sym, "int")
 
-                q_weight = q_weight[ :org_w_shape[0], :] / np.expand_dims(scales, axis=-1)
+                q_weight = q_weight[:org_w_shape[0], :] / np.expand_dims(scales, axis=-1)
                 out = np.matmul(inp, q_weight)
                 loss += np.mean(np.power((org_out - out), 2))
 
@@ -258,11 +258,11 @@ def _apply_awq_clip(model, weight_config, absorb_pairs, output_dicts, num_bits, 
                 ):  # pragma: no cover
                     # MatMulFpQ4 support 4 bits and 32 group_size with ort 1.16.0 and 1.16.1 versions
                     # MatMulNBits supports 4 bits and 2^n group_size with ort > 1.16.1
-                    q_weight = quant_utils.qdq_tensor(weight, num_bits, group_size, sym, "uint")
+                    weight = quant_utils.qdq_tensor(weight, num_bits, group_size, scheme, "uint", ratio)
                 else:
-                    q_weight = quant_utils.qdq_tensor(weight, num_bits, group_size, sym, "int")
-                q_weight = q_weight[ :org_w_shape[0], :] / np.expand_dims(scales, axis=-1)
-                out = np.matmul(inp, q_weight)
+                    weight = quant_utils.qdq_tensor(weight, num_bits, group_size, scheme, "int", ratio)
+                
+                out = np.matmul(inp, weight[:, :org_w_shape[0]])
                 loss = np.mean(np.power((org_out - cur_out), 2))
                 is_best = loss < best_error
                 if is_best:
